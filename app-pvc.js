9b16046e55c41b9df849c6eff6d03b36073b1895
Weizong Chen <weizong.chen@uk.tesco.com>
2016-08-20 15:17:43 +0100

    Add player vs computer mode

---
+++ app-pvc.js
@@ -0,0 +1,44 @@
+// Define moves inside an Array in the order of 0 > 1 > 2 > 0
+var _moves = ['Rock', 'Scissors', 'Paper'];
+var _movesLength = _moves.length;
+var comMovePos2;
+var $player1Move;
+var $result = document.getElementById('result');
+
+function genRandom() {
+	'use strict';
+	return Math.floor(Math.random() * _movesLength);
+}
+
+function compMoves() {
+	'use strict';
+	if ($player1Move === comMovePos2) {
+		$result.innerHTML = 'Draw';
+	} else if (($player1Move - comMovePos2) === -2) {
+		$result.innerHTML = 'Player Two Won!';
+	} else if (($player1Move - comMovePos2) % 2 === 0 || ($player1Move - comMovePos2) % 2 === -1) {
+		$result.innerHTML = 'Player One Won!';
+	} else {
+		$result.innerHTML = 'Player Two Won!';
+	}
+}
+
+//Computer player 2
+document.getElementById('player2').onclick = function() {
+	'use strict';
+	var $seletedOption = document.getElementById('player1Move').selectedIndex;
+	$player1Move = Number(document.getElementsByTagName('option')[$seletedOption].value);
+	console.log($seletedOption, $player1Move, comMovePos2);
+
+	if ($player1Move === -1) {
+		$result.innerHTML = 'Please make your move!';
+	} else {
+		comMovePos2 = genRandom();
+		document.getElementById('player2Move').innerHTML = _moves[comMovePos2];
+		compMoves();
+	}
+};
+
+
+
+
--- app.js
+++ app.js
@@ -1,44 +1,47 @@
+// Define moves inside an Array in the order of 0 > 1 > 2 > 0
 var _moves = ['Rock', 'Scissors', 'Paper'];
 var _movesLength = _moves.length;
 var comMovePos1;
 var comMovePos2;
 var $result = document.getElementById('result');
 
-
-function genRandom() {	
+function genRandom() {
+	'use strict';
 	return Math.floor(Math.random() * _movesLength);
-};
+}
 
 function compMoves() {
+	'use strict';
 	if (comMovePos1 === comMovePos2) {
 		$result.innerHTML = 'Draw';
 	} else if ((comMovePos1 - comMovePos2) === -2) {
 		$result.innerHTML = 'Player Two Won!';
 	} else if ((comMovePos1 - comMovePos2) % 2 === 0 || (comMovePos1 - comMovePos2) % 2 === -1) {
 		$result.innerHTML = 'Player One Won!';
-	} /*else if ((comMovePos1 - comMovePos2) % 2 === 1) {
+	} else {
 		$result.innerHTML = 'Player Two Won!';
-	}*/ else {
-		$result.innerHTML = 'Player Two Won!';
-	};
-	console.log(comMovePos1, comMovePos2);
-};
+	}
+	//console.log(comMovePos1, comMovePos2);
+}
 
-
+// Computer player 1
 document.getElementById('player1').onclick = function() {
+	'use strict';
 	comMovePos1 = genRandom();
 	document.getElementById('player1Move').innerHTML = _moves[comMovePos1];
 	if (comMovePos2 !== null && comMovePos2 !== undefined) {
 		compMoves();
-	};
+	}
 };
 
+//Computer player 2
 document.getElementById('player2').onclick = function() {
+	'use strict';
 	comMovePos2 = genRandom();
 	document.getElementById('player2Move').innerHTML = _moves[comMovePos2];
 	if (comMovePos1 !== null && comMovePos1 !== undefined) {
 		compMoves();
-	};
+	}
 };
 
 
