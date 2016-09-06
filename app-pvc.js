eca7cb9510a08529a129c66def69db16ef908c4b
Weizong Chen <weizong.chen@uk.tesco.com>
2016-09-06 21:31:00 +0100

    update compare function and make whole game extensible with more moves possible

--- app-pvc.js
+++ app-pvc.js
@@ -1,43 +1,45 @@
-// Define moves inside an Array in the order of 0 > 1 > 2 > 0
-var _moves = ['Rock', 'Scissors', 'Paper'];
-var _movesLength = _moves.length;
-var comMovePos2;
-var $player1Move;
-var $result = document.getElementById('result');
+(function() {
 
-function genRandom() {
 	'use strict';
-	return Math.floor(Math.random() * _movesLength);
-}
 
-function compMoves() {
-	'use strict';
-	if ($player1Move === comMovePos2) {
-		$result.innerHTML = 'Draw';
-	} else if (($player1Move - comMovePos2) === -2) {
-		$result.innerHTML = 'Player Two Won!';
-	} else if (($player1Move - comMovePos2) % 2 === 0 || ($player1Move - comMovePos2) % 2 === -1) {
-		$result.innerHTML = 'Player One Won!';
-	} else {
-		$result.innerHTML = 'Player Two Won!';
+	var _moves = [
+									{move: 'Rock', beats: ['Scissors']},
+									{move: 'Scissors', beats: ['Paper']},
+									{move: 'Paper', beats: ['Rock']}
+								];
+	var _movesLength = _moves.length;
+	var comMovePos2;
+	var $player1Move;
+	var $result = document.getElementById('result');
+
+	function genRandom() {
+		return Math.floor(Math.random() * _movesLength);
 	}
-}
 
-//Computer player 2
-document.getElementById('player2').onclick = function() {
-	'use strict';
-	var $seletedOption = document.getElementById('player1Move').selectedIndex;
-	$player1Move = Number(document.getElementsByTagName('option')[$seletedOption].value);
-
-	if ($player1Move === -1) {
-		$result.innerHTML = 'Please make your move!';
-	} else {
-		comMovePos2 = genRandom();
-		document.getElementById('player2Move').innerHTML = _moves[comMovePos2];
-		compMoves();
+	function compMoves() {
+		if ($player1Move === comMovePos2) {
+			$result.innerHTML = 'Draw';
+		} else if (_moves[$player1Move].beats.indexOf(_moves[comMovePos2].move) !== -1 ) {
+			$result.innerHTML = 'Player One Won!';
+		} else {
+			$result.innerHTML = 'Player Two Won!';
+		}
 	}
-};
+
+	//Computer player 2
+	document.getElementById('player2').onclick = function() {
+		var $seletedOption = document.getElementById('player1Move').selectedIndex;
+		$player1Move = Number(document.getElementsByTagName('option')[$seletedOption].value);
+
+		if ($player1Move === -1) {
+			$result.innerHTML = 'Please make your move!';
+		} else {
+			comMovePos2 = genRandom();
+			document.getElementById('player2Move').innerHTML = _moves[comMovePos2].move;
+			compMoves();
+		}
+	};
 
 
-
+}());
 
--- app.js
+++ app.js
@@ -1,46 +1,47 @@
-// Define moves inside an Array in the order of 0 > 1 > 2 > 0
-var _moves = ['Rock', 'Scissors', 'Paper'];
-var _movesLength = _moves.length;
-var comMovePos1;
-var comMovePos2;
-var $result = document.getElementById('result');
+(function() {
 
-function genRandom() {
 	'use strict';
-	return Math.floor(Math.random() * _movesLength);
-}
 
-function compMoves() {
-	'use strict';
-	if (comMovePos1 === comMovePos2) {
-		$result.innerHTML = 'Draw';
-	} else if ((comMovePos1 - comMovePos2) === -2) {
-		$result.innerHTML = 'Player Two Won!';
-	} else if ((comMovePos1 - comMovePos2) % 2 === 0 || (comMovePos1 - comMovePos2) % 2 === -1) {
-		$result.innerHTML = 'Player One Won!';
-	} else {
-		$result.innerHTML = 'Player Two Won!';
+	var _moves = [
+									{move: 'Rock', beats: ['Scissors']}, 
+									{move: 'Scissors', beats: ['Paper']},
+							  	{move: 'Paper', beats: ['Rock']}
+							  ];
+	var _movesLength = _moves.length;
+	var comMovePos1;
+	var comMovePos2;
+	var $result = document.getElementById('result');
+
+	function genRandom() {
+		return Math.floor(Math.random() * _movesLength);
 	}
-}
 
-// Computer player 1
-document.getElementById('player1').onclick = function() {
-	'use strict';
-	comMovePos1 = genRandom();
-	document.getElementById('player1Move').innerHTML = _moves[comMovePos1];
-	if (comMovePos2 !== null && comMovePos2 !== undefined) {
-		compMoves();
+	function compMoves() {
+		if (comMovePos1 === comMovePos2) {
+			$result.innerHTML = 'Draw';
+		} else if (_moves[comMovePos1].beats.indexOf(_moves[comMovePos2].move) !== -1) {
+			$result.innerHTML = 'Player One Won!';
+		} else {
+			$result.innerHTML = 'Player Two Won!';
+		}
 	}
-};
 
-//Computer player 2
-document.getElementById('player2').onclick = function() {
-	'use strict';
-	comMovePos2 = genRandom();
-	document.getElementById('player2Move').innerHTML = _moves[comMovePos2];
-	if (comMovePos1 !== null && comMovePos1 !== undefined) {
-		compMoves();
-	}
-};
+	// Computer player 1
+	document.getElementById('player1').onclick = function() {
+		comMovePos1 = genRandom();
+		document.getElementById('player1Move').innerHTML = _moves[comMovePos1].move;
+		if (comMovePos2 !== null && comMovePos2 !== undefined) {
+			compMoves();
+		}
+	};
 
+	//Computer player 2
+	document.getElementById('player2').onclick = function() {
+		comMovePos2 = genRandom();
+		document.getElementById('player2Move').innerHTML = _moves[comMovePos2].move;
+		if (comMovePos1 !== null && comMovePos1 !== undefined) {
+			compMoves();
+		}
+	};
 
+}());
