312b9a5533e434fe615bc93ed8f51dc4632ca60b
Weizong Chen <weizong.chen@uk.tesco.com>
2016-09-14 12:38:16 +0100

    convert tabs to spaces

--- app-pvc.js
+++ app-pvc.js
@@ -1,44 +1,44 @@
 (function() {
 
-	'use strict';
+  'use strict';
 
-	var _moves = [
-									{move: 'Rock', beats: ['Scissors']},
-									{move: 'Scissors', beats: ['Paper']},
-									{move: 'Paper', beats: ['Rock']}
-								];
-	var _movesLength = _moves.length;
-	var comMovePos2;
-	var $player1Move;
-	var $result = document.getElementById('result');
+  var _moves = [
+                  {move: 'Rock', beats: ['Scissors']},
+                  {move: 'Scissors', beats: ['Paper']},
+                  {move: 'Paper', beats: ['Rock']}
+                ];
+  var _movesLength = _moves.length;
+  var comMovePos2;
+  var $player1Move;
+  var $result = document.getElementById('result');
 
-	function genRandom() {
-		return Math.floor(Math.random() * _movesLength);
-	}
+  function genRandom() {
+    return Math.floor(Math.random() * _movesLength);
+  }
 
-	function compMoves() {
-		if ($player1Move === comMovePos2) {
-			$result.innerHTML = 'Draw';
-		} else if (_moves[$player1Move].beats.indexOf(_moves[comMovePos2].move) !== -1 ) {
-			$result.innerHTML = 'Player One Won!';
-		} else {
-			$result.innerHTML = 'Player Two Won!';
-		}
-	}
+  function compMoves() {
+    if ($player1Move === comMovePos2) {
+      $result.innerHTML = 'Draw';
+    } else if (_moves[$player1Move].beats.indexOf(_moves[comMovePos2].move) !== -1 ) {
+      $result.innerHTML = 'Player One Won!';
+    } else {
+      $result.innerHTML = 'Player Two Won!';
+    }
+  }
 
-	//Computer player 2
-	document.getElementById('player2').onclick = function() {
-		var $seletedOption = document.getElementById('player1Move').selectedIndex;
-		$player1Move = Number(document.getElementsByTagName('option')[$seletedOption].value);
+  //Computer player 2
+  document.getElementById('player2').onclick = function() {
+    var $seletedOption = document.getElementById('player1Move').selectedIndex;
+    $player1Move = Number(document.getElementsByTagName('option')[$seletedOption].value);
 
-		if ($player1Move === -1) {
-			$result.innerHTML = 'Please make your move!';
-		} else {
-			comMovePos2 = genRandom();
-			document.getElementById('player2Move').innerHTML = _moves[comMovePos2].move;
-			compMoves();
-		}
-	};
+    if ($player1Move === -1) {
+      $result.innerHTML = 'Please make your move!';
+    } else {
+      comMovePos2 = genRandom();
+      document.getElementById('player2Move').innerHTML = _moves[comMovePos2].move;
+      compMoves();
+    }
+  };
 
 
 }());
--- app.js
+++ app.js
@@ -1,47 +1,47 @@
 (function() {
 
-	'use strict';
+  'use strict';
 
-	var _moves = [
-									{move: 'Rock', beats: ['Scissors']}, 
-									{move: 'Scissors', beats: ['Paper']},
-							  	{move: 'Paper', beats: ['Rock']}
-							  ];
-	var _movesLength = _moves.length;
-	var comMovePos1;
-	var comMovePos2;
-	var $result = document.getElementById('result');
+  var _moves = [
+                  {move: 'Rock', beats: ['Scissors']}, 
+                  {move: 'Scissors', beats: ['Paper']},
+                  {move: 'Paper', beats: ['Rock']}
+                ];
+  var _movesLength = _moves.length;
+  var comMovePos1;
+  var comMovePos2;
+  var $result = document.getElementById('result');
 
-	function genRandom() {
-		return Math.floor(Math.random() * _movesLength);
-	}
+  function genRandom() {
+    return Math.floor(Math.random() * _movesLength);
+  }
 
-	function compMoves() {
-		if (comMovePos1 === comMovePos2) {
-			$result.innerHTML = 'Draw';
-		} else if (_moves[comMovePos1].beats.indexOf(_moves[comMovePos2].move) !== -1) {
-			$result.innerHTML = 'Player One Won!';
-		} else {
-			$result.innerHTML = 'Player Two Won!';
-		}
-	}
+  function compMoves() {
+    if (comMovePos1 === comMovePos2) {
+      $result.innerHTML = 'Draw';
+    } else if (_moves[comMovePos1].beats.indexOf(_moves[comMovePos2].move) !== -1) {
+      $result.innerHTML = 'Player One Won!';
+    } else {
+      $result.innerHTML = 'Player Two Won!';
+    }
+  }
 
-	// Computer player 1
-	document.getElementById('player1').onclick = function() {
-		comMovePos1 = genRandom();
-		document.getElementById('player1Move').innerHTML = _moves[comMovePos1].move;
-		if (comMovePos2 !== null && comMovePos2 !== undefined) {
-			compMoves();
-		}
-	};
+  // Computer player 1
+  document.getElementById('player1').onclick = function() {
+    comMovePos1 = genRandom();
+    document.getElementById('player1Move').innerHTML = _moves[comMovePos1].move;
+    if (comMovePos2 !== null && comMovePos2 !== undefined) {
+      compMoves();
+    }
+  };
 
-	//Computer player 2
-	document.getElementById('player2').onclick = function() {
-		comMovePos2 = genRandom();
-		document.getElementById('player2Move').innerHTML = _moves[comMovePos2].move;
-		if (comMovePos1 !== null && comMovePos1 !== undefined) {
-			compMoves();
-		}
-	};
+  //Computer player 2
+  document.getElementById('player2').onclick = function() {
+    comMovePos2 = genRandom();
+    document.getElementById('player2Move').innerHTML = _moves[comMovePos2].move;
+    if (comMovePos1 !== null && comMovePos1 !== undefined) {
+      compMoves();
+    }
+  };
 
 }());
